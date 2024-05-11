7d08b72d3d5f40943235b23ab65579f975195e24
Ludwig Hubert <ludwig.hubert@gmail.com>
2024-05-11 21:23:13 +0200

    feat: add chevron and lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,21 +14,29 @@
 	>
 		<h1 class="h1 uppercase font-bold">1. Trail Fun Run</h1>
 		<p>
-   Servus, liebe Bergfexn! Seids gegrüßt zum Trail Fun Run, dem entspannten Event für alle, die
+			Servus, liebe Bergfexn! Seids gegrüßt zum Trail Fun Run, dem entspannten Event für alle, die
 			die Berge lieben und das Berglaufen in der Natur genießen wollen. Schnappt euch eure
 			Lederhosen und Wanderschuhe, denn hier erwartet euch ein Tag voller Gaudi und Abenteuer! Wir
-			haben ein paar zünftige Trails für euch vorbereitet, von klein bis groß ist alles dabei! Freuts euch
-			auf atemberaubende Ausblicke, eine anspornende Atmosphäre und natürlich jede Menge Spaß.<br />Mia
-			gfrei'n uns auf eich, des wird a Gaudi!
+			haben ein paar zünftige Trails für euch vorbereitet, von klein bis groß ist alles dabei!
+			Freuts euch auf atemberaubende Ausblicke, eine anspornende Atmosphäre und natürlich jede Menge
+			Spaß.<br />Mia gfrei'n uns auf eich, des wird a Gaudi!
 		</p>
 		<h2 class="h2 uppercase font-bold">Das Event</h2>
 		<p>
-			Das Ziel ist, Spaß zu haben! Kein Regelwerk. Kein Stress. Kein Trubel. Wir laufen einfach frühs los
-			(je nach Distanz früher oder später) und kommen nachmittags wieder am Parkplatz an. Dort gibt's dann Verpflegung,
-			Getränke und einen See zum runterkühlen. Das Datum suchen wir gemeinsam aus. Klingt gut? Dann meld dich an!
+			Das Ziel ist, Spaß zu haben! Kein Regelwerk. Kein Stress. Kein Trubel. Wir laufen einfach
+			frühs los (je nach Distanz früher oder später) und kommen nachmittags wieder am Parkplatz an.
+			Dort gibt's dann Verpflegung, Getränke und einen See zum runterkühlen. Das Datum suchen wir
+			gemeinsam aus. Klingt gut? Dann meld dich an!
 		</p>
-		<a href="https://docs.google.com/forms/d/e/1FAIpQLSem6Ry7K2Gn3toIl8yXz1DQbLPSQT9RS9nVC3NvM5oSs7LT5A/viewform?usp=sf_link" class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded">
-			Zur Anmeldung
+		<a
+			href="https://docs.google.com/forms/d/e/1FAIpQLSem6Ry7K2Gn3toIl8yXz1DQbLPSQT9RS9nVC3NvM5oSs7LT5A/viewform?usp=sf_link"
+			class="bg-primary-500 hover:bg-primary-700 text-white font-bold py-2 px-4 rounded"
+			target="_blank"
+		>
+			Zur Anmeldung <span
+				class="inline-block w-3 h-3 border-inherit border-r-4 border-b-4 transform -rotate-45 -translate-x-1/2 ml-1"
+			>
+			</span>
 		</a>
 		<!-- <section class="text-center"><Countdown /></section> -->
 		<h2 class="h2 uppercase font-bold">Die Strecken</h2>
